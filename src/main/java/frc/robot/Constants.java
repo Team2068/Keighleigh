305354bd50b6132976c12cd03bcd728daf4fe34b
keiraSchoolcraft <43482081+keiraSchoolcraft@users.noreply.github.com>
2022-02-24 15:38:50 -0500

    Shooter and Aimbot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,24 +4,16 @@
 
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
 public final class Constants {
+    public static final int CURRENT_LIMIT = 30;
+
     /**
      * The left-to-right distance between the drivetrain wheels
-     *
      * Should be measured from center to center.
      */
     public static final double DRIVETRAIN_TRACKWIDTH_METERS = 21.5; // FIXME Measure and set trackwidth
     /**
      * The front-to-back distance between the drivetrain wheels.
-     *
      * Should be measured from center to center.
      */
     public static final double DRIVETRAIN_WHEELBASE_METERS = 19.5; // FIXME Measure and set wheelbase
@@ -47,4 +39,25 @@ public final class Constants {
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 8; // FIXME Set back right steer motor ID
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 0; // FIXME Set back right steer encoder ID
     public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set back right steer offset
+
+    
+    public final static class ShooterConstants{
+        public final static int FLYWHEEL_1 = 3;
+        public final static int FLYWHEEL_2 = 6;
+    }
+
+    public final static class GameElementConstants {
+        public final static double UPPER_HUB = 2.64; //meters
+        public final static double LOWER_HUB = 1.04; //meters
+        public final static double HIGH_HEIGHT = UPPER_HUB; //Subtract by robot height
+        public final static double LOW_HEIGHT = LOWER_HUB;
+      }
+    
+    public final static class AimbotConstants{
+        public static final double baseSpeed = 0.5;
+        public static final double minimumAdjustment = 1.1;
+        public static final double Kp = 5;
+        public static final double Ki = 0;
+        public static final double Kd = 0.8;
+    }
 }
