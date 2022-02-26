60210c6e785217da7e00049c8eb1bbb3af6d6afc
keiraSchoolcraft <43482081+keiraSchoolcraft@users.noreply.github.com>
2022-02-26 09:36:54 -0500

    Fix Limelight to get Data

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -4,11 +4,13 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
   // Diet Limelight | remove entire section during merge
-  public Limelight() {}
+  // its a limeLITE!!! :D - drew
 
   private TargetData targetData = new TargetData();
 
@@ -16,7 +18,13 @@ public class Limelight extends SubsystemBase {
     public double horizontalOffset = 0; // Horizontal Offset From Crosshair To Target -29.8 to 29.8 degrees
   }
 
-  public TargetData getTargetData(){
-  return targetData;
+  public TargetData getTargetData() {
+    return targetData;
+  }
+
+  @Override
+  public void periodic() {
+    // updating in periodic since we don't need more than this at the moment
+    targetData.horizontalOffset = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0.0);
   }
 }
