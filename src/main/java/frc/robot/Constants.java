e1cdf80e14a4671ec81b276e2cc7dfd3c9311651
Keira Schoolcraft <keira.schoolcraft@icloud.com>
2022-02-28 14:33:59 -0500

    constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,13 +6,13 @@ package frc.robot;
 
 public final class Constants {
 
-    public final static double ROBOT_HEIGHT = 0.0;//adjust
+    public final static double ROBOT_HEIGHT = 0.0;// adjust
 
-    public final static class ShooterConstants{
+    public final static class ShooterConstants {
         public final static int FLYWHEEL_1 = 14;
         public final static int FLYWHEEL_2 = 15;
     }
-   
+
     /**
      * 
      * The left-to-right distance between the drivetrain wheels
@@ -27,48 +27,58 @@ public final class Constants {
     public static final double NORMAL_SPEED = .5;
     public static final double DRIVETRAIN_WHEELBASE_METERS = 19.5; // FIXME Measure and set wheelbase
 
-    
-
     // public final static class GameElementConstants {
-    //     public final static double UPPER_HUB = 2.64; //meters
-    //     public final static double LOWER_HUB = 1.04; //meters
-    //     public final static double HIGH_HEIGHT = UPPER_HUB - ROBOT_HEIGHT;
-    //     public final static double LOW_HEIGHT = LOWER_HUB - ROBOT_HEIGHT;
-    //   }
-    // public final static class IntakeConstants{
-    // public static final int CONVEYER_MOTOR_ONE = 10;
-    // public static final int CONVERYER_TWO = 11;
-    // public static final int INTAKE = 12;
-    // public static final double Intakespeed = .2;
-    // public static final double SPIT_OUT_BALL = -.2;
+    // public final static double UPPER_HUB = 2.64; //meters
+    // public final static double LOWER_HUB = 1.04; //meters
+    // public final static double HIGH_HEIGHT = UPPER_HUB - ROBOT_HEIGHT;
+    // public final static double LOW_HEIGHT = LOWER_HUB - ROBOT_HEIGHT;
     // }
+    public final static class ConveyorConstants {
+        public static final int LOWER_CONVEYOR = 16;
+        public static final int UPPER_CONVEYOR = 17;
+    }
+    public final static class IntakeConstants {
+        //intake things
+        public static final int INTAKE_MOTOR = 18;
+        public static final double Intakespeed = .2;
+        public static final double SPIT_OUT_BALL = -.2;
+        //pneumatics things
+        public static final int FORWARD_CHANNEL = 1;
+        public static final int REVERSE_CHANNEL = 0;
+    }
+
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 7; // FIXME Set front left module drive motor ID
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 6; // FIXME Set front left module steer motor ID
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 13; // FIXME Set front left steer encoder ID
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(75.0); // FIXME Measure and set front left steer offset
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(75.0); // FIXME Measure and set front
+                                                                                       // left steer offset
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 5; // FIXME Set front right drive motor ID
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 4; // FIXME Set front right steer motor ID
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 12; // FIXME Set front right steer encoder ID
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(153); // FIXME Measure and set front right steer offset
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(153); // FIXME Measure and set front
+                                                                                       // right steer offset
 
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 9; // FIXME Set back left drive motor ID
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 8; // FIXME Set back left steer motor ID
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 10; // FIXME Set back left steer encoder ID
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(295); // FIXME Measure and set back left steer offset
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(295); // FIXME Measure and set back left
+                                                                                     // steer offset
 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 3; // FIXME Set back right drive motor ID
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 2; // FIXME Set back right steer motor ID
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11; // FIXME Set back right steer encoder ID
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(5); // FIXME Measure and set back right steer offset
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(5); // FIXME Measure and set back right
+                                                                                    // steer offset
+
     public final static class GameElementConstants {
-        public final static double UPPER_HUB = 2.64; //meters
-        public final static double LOWER_HUB = 1.04; //meters
-        public final static double HIGH_HEIGHT = UPPER_HUB; //Subtract by robot height
+        public final static double UPPER_HUB = 2.64; // meters
+        public final static double LOWER_HUB = 1.04; // meters
+        public final static double HIGH_HEIGHT = UPPER_HUB; // Subtract by robot height
         public final static double LOW_HEIGHT = LOWER_HUB;
-      }
-    
-    public final static class AimbotConstants{
+    }
+
+    public final static class AimbotConstants {
         public static final double baseSpeed = 0.5;
         public static final double minimumAdjustment = 1.1;
         public static final double Kp = 5;
@@ -77,4 +87,3 @@ public final class Constants {
     }
 
 }
-
