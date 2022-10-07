4d3545dc4d031bd9a4e699242fc8e629f5babf40
Red <redstonegameraa@gmail.com>
2022-10-07 15:17:42 +0000

    lerp 5 point limit removed

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -112,15 +112,14 @@ public class Limelight extends SubsystemBase {
     int low = 0;
     int high = 0;
 
-    // If needed, replace the constants like 2 and 4 with distTab.lenght and an operation to return the needed value
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < distTab.length>>1; i++) {
       if (distance < distTab[i]){ //If lower > dist -> upper bound found
         high = i; 
         low = i-1;
         break;
       }
 
-      if (distance > distTab[4 - i]){ //If higher < dist -> higer is lower bound
+      if (distance > distTab[distTab.length - 1- i]){ //If higher < dist -> higer is lower bound
         low = 4 - i ;
         high = low + 1;
         break;
@@ -130,7 +129,7 @@ public class Limelight extends SubsystemBase {
     if (low == -1)
       return rpmTab[0]+(distance-distTab[0])/(distTab[1]-distTab[0])*(rpmTab[1]-rpmTab[0]);
     
-    if (high >= 4)
+    if (high == distTab.length)
       return rpmTab[3]+(distance-distTab[3])/(distTab[4]-distTab[3])*(rpmTab[4]-rpmTab[3]);
     
     return rpmTab[low] + (distance - distTab[low])*((rpmTab[high]- rpmTab[low])/(distTab[high]- distTab[low]));
