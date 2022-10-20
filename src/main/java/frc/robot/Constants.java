c921b82ff8f842485a48ced4d2a47b01bd5cc7a3
keiraSchoolcraft <43482081+keiraSchoolcraft@users.noreply.github.com>
2022-10-20 15:14:35 -0400

    update paths

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -202,10 +202,15 @@ public final class Constants {
     // }
 
     public static final class TrajectoryPaths {
-        public static final Trajectory FourBallRed_BackUpToFirstBall = PathPlanner.loadPath("4BR_BackUpToFirstBall", 1, 1);
-        public static final Trajectory FourBallRed_GoToHumanPlayer = PathPlanner.loadPath("4BR_GoToHumanPlayer", 1, 1);
-        public static final Trajectory FourBallRed_Shoot = PathPlanner.loadPath("4BR_Shoot", 1, 1);
-        public static final Trajectory FourBallRed_Little = PathPlanner.loadPath("4BR_GoSlightlyFarther", 1, 1);
-        public static final Trajectory TestPath = PathPlanner.loadPath("Vaccum", 1, 1);
+        // public static final Trajectory FourBallRed_BackUpToFirstBall = PathPlanner.loadPath("4BR_BackUpToFirstBall", 1, 1);
+        // public static final Trajectory FourBallRed_GoToHumanPlayer = PathPlanner.loadPath("4BR_GoToHumanPlayer", 1, 1);
+        // public static final Trajectory FourBallRed_Shoot = PathPlanner.loadPath("4BR_Shoot", 1, 1);
+        // public static final Trajectory FourBallRed_Little = PathPlanner.loadPath("4BR_GoSlightlyFarther", 1, 1);
+        // public static final Trajectory TestPath = PathPlanner.loadPath("Vaccum", 1, 1);
+        public static final Trajectory BackToCollectBall = PathPlanner.loadPath ("BackToCollectBall", 1, 1);
+        public static final Trajectory Turn1 = PathPlanner.loadPath("Turn1", 1, 1); 
+        public static final Trajectory Step1 = PathPlanner.loadPath("Step1", 1, 1);
+        public static final Trajectory Step2 = PathPlanner.loadPath("Step2", 1, 1);
+       public static final Trajectory Step3 = PathPlanner.loadPath("Step3", 1, 1);
     }
 }
--- src/main/java/frc/robot/commands/SixBallAutoBlue.java
+++ src/main/java/frc/robot/commands/SixBallAutoBlue.java
@@ -3,6 +3,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
 import frc.robot.subsystems.ConveyorSubsystem;
 import frc.robot.subsystems.DrivetrainSubsystem;
@@ -11,25 +12,27 @@ import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class SixBallAutoBlue extends SequentialCommandGroup {
-    public SixBallAutoBlue(IntakeSubsystem intakeSubsystem, Limelight limelight, DrivetrainSubsystem drivetrainSubsystem, ShooterSubsystem shooterSubsystem, ConveyorSubsystem conveyorSubsystem) {
-        super(
+  public SixBallAutoBlue(IntakeSubsystem intakeSubsystem, Limelight limelight, DrivetrainSubsystem drivetrainSubsystem,
+      ShooterSubsystem shooterSubsystem, ConveyorSubsystem conveyorSubsystem) {
+    super(
         new SequentialCommandGroup(
-          new ParallelCommandGroup(
+            new InstantCommand(intakeSubsystem::deployIntake),
+            new ParallelCommandGroup(
                 new Paths(frc.robot.Constants.TrajectoryPaths.BackToCollectBall, drivetrainSubsystem),
                 new InstantCommand(intakeSubsystem::intakeBall)),
-                new Paths(frc.robot.Constants.TrajectoryPaths.Turn1, drivetrainSubsystem),
-                new AimAndFire(shooterSubsystem, conveyorSubsystem, limelight, drivetrainSubsystem),
-                new ParallelCommandGroup(
-                  new Paths(frc.robot.Constants.TrajectoryPaths.Step1, drivetrainSubsystem),
-                  new InstantCommand(intakeSubsystem::intakeBall)),
-                new AimAndFire(shooterSubsystem, conveyorSubsystem, limelight, drivetrainSubsystem),
-                new ParallelCommandGroup(
-                  new Paths(frc.robot.Constants.TrajectoryPaths.Step2, drivetrainSubsystem),
-                  new InstantCommand(intakeSubsystem::intakeBall)),
-                new ParallelCommandGroup(
-                  new Paths(frc.robot.Constants.TrajectoryPaths.Step3, drivetrainSubsystem),
-                  new InstantCommand(intakeSubsystem::intakeBall)),
-                new AimAndFire(shooterSubsystem, conveyorSubsystem, limelight, drivetrainSubsystem)
-                ));
-    }
+            new AimAndFire(shooterSubsystem, conveyorSubsystem, limelight, drivetrainSubsystem),
+            new InstantCommand(intakeSubsystem::retractIntake),
+            new Paths(frc.robot.Constants.TrajectoryPaths.Step1, drivetrainSubsystem),
+            new InstantCommand(intakeSubsystem::deployIntake),
+            new WaitCommand(2),
+            new InstantCommand(intakeSubsystem::intakeBall),
+            new AimAndFire(shooterSubsystem, conveyorSubsystem, limelight, drivetrainSubsystem)));
+            // new ParallelCommandGroup(
+            //     new Paths(frc.robot.Constants.TrajectoryPaths.Step2, drivetrainSubsystem),
+            //     new InstantCommand(intakeSubsystem::intakeBall)),
+            // new ParallelCommandGroup(
+            //     new Paths(frc.robot.Constants.TrajectoryPaths.Step3, drivetrainSubsystem),
+            //     new InstantCommand(intakeSubsystem::intakeBall)),
+            // new AimAndFire(shooterSubsystem, conveyorSubsystem, limelight, drivetrainSubsystem)));
+  }
 }
